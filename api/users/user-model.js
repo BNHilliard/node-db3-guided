de4c5a7a2526dcaa63e1dbbf003478f09d8012f3
timothy hoover <bnhilliard>
2022-11-22 21:47:37 -0800

    update

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -9,6 +9,12 @@ module.exports = {
 }
 
 function findPosts(user_id) {
+return db('posts')
+.select('contents', 'posts.id as post_id')
+.where('user_id', user_id)
+
+// JOIN users ON posts.user_id = users.id
+.join('users', 'posts.user_id', 'users.id')
   /*
     Implement so it resolves this structure:
 
@@ -25,6 +31,11 @@ function findPosts(user_id) {
 
 function find() {
   return db('users')
+  .leftJoin('posts', 'user_id', 'users.id')
+  .groupBy('users.id')
+  .count('posts.id as post_count')
+  .select('users.id as user_id', 'username')
+  
   /*
     Improve so it resolves this structure:
 
@@ -44,8 +55,35 @@ function find() {
   */
 }
 
-function findById(id) {
-  return db('users').where({ id }).first()
+async function findById(id) {
+  // return db('users').where({ id }).first()
+
+  //select * from users
+  //left join posts on useres.id = user_id
+  //where users.id = id
+const result = await db('users')
+.leftJoin('posts', 'users.id', 'user_id')
+.where('users.id', id)
+.select('username', 'users.id as user_id', 'posts.id as post_id', 'contents');
+
+if (result.length === 0) {
+return null;
+}
+
+const user = {
+  username: result[0].username,
+  user_id: result[0].user_id,
+  posts: []
+}
+
+for (const row of result) {
+  if(row.post_id != null) {
+  user.posts.push({post_id: row.post_id, contents: row.contents })
+}}
+
+return user;
+
+
   /*
     Improve so it resolves this structure:
 
